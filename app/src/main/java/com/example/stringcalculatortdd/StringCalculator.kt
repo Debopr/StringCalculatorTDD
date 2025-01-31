e4afc5b65c58f33923fd998136dfcaf93e859804
deboprasad <deboprasad.mukherjee@valtech.com>
2025-01-31 12:07:51 +0530

    Test: Handle two comma-separated numbers

--- app/src/main/java/com/example/stringcalculatortdd/StringCalculator.kt
+++ app/src/main/java/com/example/stringcalculatortdd/StringCalculator.kt
@@ -3,6 +3,6 @@ package com.example.stringcalculatortdd
 class StringCalculator {
     fun add(numbers: String): Int {
         if (numbers.isEmpty()) return 0
-        return numbers.toInt()
+        return  numbers.split(",").map { it.toInt() }.sum()
     }
 }
--- app/src/test/java/com/example/stringcalculatortdd/StringCalculatorTest.kt
+++ app/src/test/java/com/example/stringcalculatortdd/StringCalculatorTest.kt
@@ -14,4 +14,9 @@ class StringCalculatorTest {
     fun `returns the number itself when only one number is passed`() {
         assertEquals(2, calculator.add("2"))
     }
+    @Test
+    fun `returns the sum of two numbers`() {
+        assertEquals(4, calculator.add("1,3"))
+    }
+
 }
